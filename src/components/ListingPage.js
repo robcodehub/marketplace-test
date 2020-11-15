3b16dc427c783ee185ac8fabfb8f9b3bb8a8ba2e
Robert Wise <robcodehub@gmail.com>
2020-11-16 10:18:10 +1100

    added additional information and styled on business listing page

--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -4,9 +4,11 @@ import {Link, useParams} from 'react-router-dom';
 import axios from 'axios';
 
 import styled, {createGlobalStyle} from 'styled-components';
-import {BurstSale, BurstNew} from '@styled-icons/foundation'
+import {BurstSale, BurstNew} from '@styled-icons/foundation';
 
-import {Handshake, CommentsDollar, Trademark, HandHoldingUsd} from '@styled-icons/fa-solid'
+import {Handshake, CommentsDollar, Trademark, HandHoldingUsd} from '@styled-icons/fa-solid';
+import {RightArrowAlt} from '@styled-icons/boxicons-regular';
+
 import { ListingsHome } from '../Listings';
 // import BadgeDollar from '@styled-icons/boxicon-solid'
 // import CommentsDollar from '@styled-icons/fa-solid'
@@ -23,7 +25,7 @@ import { ListingsHome } from '../Listings';
 const LayoutGrid = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr;
-grid-template-rows: 15% 10% 20% 20% 20% 15%;
+grid-template-rows: 15% 12% 20% 20% 5% 5% 5% 5% 15%;
 margin-top: 5rem;
 `
 
@@ -31,7 +33,7 @@ const HeadingLayout = styled.div`
 grid-row-start: 1;
 grid-row-end: 1;
 grid-column-start: 1;
-grid-column-end: 5;
+grid-column-end: span 4;
 background-color: #3c79cb;
 color: #FFF;
 `
@@ -40,9 +42,11 @@ const UnlockLayout = styled.div`
 grid-row-start: 2;
 grid-row-end: 2;
 grid-column-start: 1;
-grid-column-end: 4;
+grid-column-end: span 4;
 margin-top: 1rem;
 margin-bottom: 1rem;
+padding-bottom: 2rem;
+border-bottom: 1px solid #848a93;
 `
 
 const LeftColumnLeft = styled.div`
@@ -69,9 +73,11 @@ const ListingSummaryLayout = styled.div`
 grid-row-start: 4;
 grid-row-end: 4;
 grid-column-start: 1;
-grid-column-end: span 2;
+grid-column-end: span 4;
 margin-top: 2rem;
+margin: 2rem;
 padding-top: 2rem;
+padding-bottom: 2rem;
 `
 
 const RightLayout = styled.div`
@@ -82,12 +88,32 @@ grid-column-end: span 2;
 `
 
 
+
+const OtherInfoSectionOne = styled.div`
+grid-row-start: 5;
+grid-row-end: 5;
+grid-column-start: 1;
+grid-column-end: span 4;
+margin-top: 2rem;
+padding-top: 2rem;
+margin-bottom: 2rem;
+padding-bottom: 2rem;
+border-bottom: 1px solid #848a93;
+border-top: 1px solid #848a93;
+`
+
+const SpanWrapper = styled.div`
+  border-top: 1px solid #848a93;
+  margin: 2rem;
+`
+
 const OtherInfoLeft = styled.div`
 grid-row-start: 5;
 grid-row-end: 5;
 grid-column-start: 1;
 grid-column-end: span 2;
-margin-top: 1rem;
+margin-top: 2rem;
+padding-top: 2rem;
 margin-bottom: 2rem;
 padding-bottom: 2rem;
 `
@@ -97,7 +123,8 @@ grid-row-start: 5;
 grid-row-end: 5;
 grid-column-start: 3;
 grid-column-end: span 2;
-margin-top: 1rem;
+margin-top: 2rem;
+padding-top: 2rem;
 margin-bottom: 2rem;
 padding-bottom: 2rem;
 `
@@ -138,6 +165,9 @@ padding: 0.5em;
 
 const ListStyle = styled.ul`
   list-style-type: none;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  line-height:1.6;
 `
 
 
@@ -150,11 +180,30 @@ const NewIcon = styled(BurstNew)`
   color: green;
 `
 
+const RightArrowIcon = styled(RightArrowAlt)`
+  color: ${props => props.color || "#FFF"};
+  height: 2em;
+  width: 2em;
+`
+
 export const Grid = styled.div`
 font-family: 'Helvetica';
 `;
 
-
+const ButtonTemplate = styled.button`
+padding-top: 1rem;
+padding-bottom: 1rem;
+padding-left: 1.2rem;
+padding-right: 1.2rem;
+color: #FFF;
+font-size: 1em;
+background-color: ${props => props.color || "#3c79cb"};
+font-weight: bold;
+margin-left: 1.5rem;
+border: none;
+border-radius: 5px;
+margin-bottom: 1rem;
+`
 
 
 
@@ -224,7 +273,8 @@ export const ListingPage = () => {
       <h3>Home {'>'} Marketplace {'>'}  {listing.listing_number}</h3>
       </HeadingLayout>
       <UnlockLayout>
-      <h3>Unlock a listing for full access to the URL, Google Analytic, Profit and Loss Statement, etc. </h3>
+      <h3>Unlock a listing for full access to the URL, Google Analytic, Profit and Loss Statement, etc.  <ButtonTemplate>Buy Now <RightArrowIcon /></ButtonTemplate> <ButtonTemplate color={"#f5a622"}>Unlock Listing <RightArrowIcon /></ButtonTemplate>
+      </h3>
       </UnlockLayout>
 
           <LeftColumnLeft>
@@ -263,6 +313,85 @@ export const ListingPage = () => {
           </ListStyle>
           {listing.has_trademark ? <TrademarkIcon />: <></>}
         </RightLayout>
+
+        <OtherInfoLeft>
+        <SpanWrapper>
+          <h2>Opportunities</h2>
+          <ListStyle>
+          {listing.opportunities.map((opp) => {
+          return <li>{opp}</li>
+          })
+          }
+          </ListStyle>
+          </SpanWrapper>
+          <SpanWrapper>
+        <h2>Work {'&'} Skills Required</h2>
+          <ListStyle>
+          {listing.work_required.map((skill) => {
+          return <li>{skill}</li>
+          })
+          }
+          </ListStyle>
+          </SpanWrapper>
+          <SpanWrapper>
+          <h2>Seller Support</h2>
+        <p>{listing.seller_support}</p>
+        </SpanWrapper>
+        <SpanWrapper>
+          <h2>Other Information</h2>
+          <ListStyle>
+
+
+        <li>Work Required Per Week: {listing.hours_worked_per_week} Hours</li>
+        <li>Private Blog Network(PBN): {listing.uses_pbn ? "Yes": "No"}</li>
+        <li>Domain Type:{' '}
+          {listing.sites.map((site) => {
+          return <>.{site.domain_type}</>
+          })
+          }
+
+          </li>
+        <li>Platform:{' '}
+          {listing.sites.map((site) => {
+          return <>{site.platform}</>
+          })
+          }
+
+        </li>
+          </ListStyle>
+          </SpanWrapper>
+        </OtherInfoLeft>
+          <OtherInfoRight>
+          <SpanWrapper>
+          <h2>Risks</h2>
+          <ListStyle>
+          {listing.risks.map((risk) => {
+          return <li>{risk}</li>
+          })
+          }
+          </ListStyle>
+          </SpanWrapper>
+          <SpanWrapper>
+          <div>
+            <h2>Social Media Channels</h2>
+            <ButtonTemplate>Unlock Listing<RightArrowIcon /></ButtonTemplate>
+          </div>
+          </SpanWrapper>
+          <SpanWrapper>
+          <h2>Reason for Sale</h2>
+          <p>{listing.reason_for_sale}</p>
+          </SpanWrapper>
+          <SpanWrapper>
+          <h2>Buyer Profiles</h2>
+          <ListStyle>
+          {listing.buyer_profiles.map((profile) => {
+          return <li>{profile}</li>
+          })
+          }
+          </ListStyle>
+          </SpanWrapper>
+          </OtherInfoRight>
+
         </LayoutGrid>
         </>
 
